f095f65d62f74e0ea6b3662f91b2604c70f63470
Michael Gauthier <mike@silverorange.com>
2021-04-21 12:00:45 -0300

    Fix missing functions in header

--- native/sqlc.h
+++ native/sqlc.h
@@ -51,7 +51,9 @@ sqlc_handle_t sqlc_db_open(const char *filename, int flags);
 /* Custom tokenizers */
 sqlc_handle_t sqlc_syn_context_create(sqlc_handle_t db);
 void sqlc_syn_context_delete(sqlc_handle_t syn_context_h);
-int sqlc_tokenizer_register_all(sqlc_handle_t db, sqlc_handle_t syn_context_h);
+sqlc_handle_t sqlc_stp_context_create(sqlc_handle_t db);
+void sqlc_stp_context_delete(sqlc_handle_t stp_context_h);
+int sqlc_tokenizer_register_all(sqlc_handle_t db, sqlc_handle_t syn_context_h, sqlc_handle_t stp_context_h);
 
 // FUTURE TBD (???):
 //sqlc_handle_t sqlc_db_open_vfs(const char *filename, int flags, const char *vfs);
