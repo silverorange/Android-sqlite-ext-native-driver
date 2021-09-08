b9ac48e8d1a9fe49a3074b8246110b46ff554d6f
Michael Gauthier <mike@silverorange.com>
2021-09-07 21:26:51 -0300

    Update min target SDK

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,4 +3,6 @@
 APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
 
 # Explicit setting ref: https://developer.android.com/ndk/guides/stable_apis
-APP_PLATFORM := android-14
+# Mike: I bumped this from 14 to 21 as that is the minimum SDK we target.
+# See also https://github.com/OutSystems/Android-sqlite-native-driver/pull/1/files#r530635884
+APP_PLATFORM := android-21
